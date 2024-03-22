7114b08beadda91dfb4ff0367f52d798093d623a
Sam <samvelavagyan91@gmail.com>
2024-03-23 02:28:34 +0400

    add file version change and build value change's file's

---
+++ docker-compose-file-backend-build-value-change.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+COMPOSE_FILE="docker-compose.yml"
+SERVICE_NAME="api"
+NEW_BUILD_VALUE=$1
+
+sed -i "/^\s*$SERVICE_NAME:/{:a;n;/^\s*build:/s|^\s*build:.*|    build: $NEW_BUILD_VALUE|;Ta}" "$COMPOSE_FILE"
