61fe237fa62937bbe5226066e630033403c8bafa
Sam <samvelavagyan91@gmail.com>
2024-03-23 07:50:42 +0400

    change functionality of images update

--- docker-compose-file-backend-build-value-change.sh
+++ docker-compose-file-backend-build-value-change.sh
@@ -4,4 +4,4 @@ COMPOSE_FILE="docker-compose.yml"
 SERVICE_NAME="api"
 NEW_BUILD_VALUE=$1
 
-sed -i "/^\s*$SERVICE_NAME:/{:a;n;/^\s*build:/s|^\s*build:.*|    build: $NEW_BUILD_VALUE|;Ta}" "$COMPOSE_FILE"
+sed -i "s|^\s*image:.*|    image: $NEW_IMAGE|" "$COMPOSE_FILE"
