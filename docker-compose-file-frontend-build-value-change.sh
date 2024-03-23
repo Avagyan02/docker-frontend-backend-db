4313f550019304df42e7d4dd81075ee4fce3a616
Sam <samvelavagyan91@gmail.com>
2024-03-23 08:03:51 +0400

    change functionality of images update

--- docker-compose-file-frontend-build-value-change.sh
+++ docker-compose-file-frontend-build-value-change.sh
@@ -4,4 +4,4 @@ COMPOSE_FILE="docker-compose.yml"
 SERVICE_NAME="web"
 NEW_BUILD_VALUE=$1
 
-sed -i "s|^\s*image:.*|    image: $NEW_IMAGE|" "$COMPOSE_FILE"
+sed -i "/^\s*$SERVICE_NAME:/{:a;n;/^\s*image:/s|^\s*image:.*|    image: $NEW_BUILD_VALUE|;Ta}" "$COMPOSE_FILE"
